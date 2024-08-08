8f20fb873a9372dba6ac246cdcb3cf3ee2d26ba2
Vicente Salinas <vicentesalinas@Vicentes-Laptop.attlocal.net>
2024-08-08 15:39:15 -0500

    Proyecto 7 primera version de entrega

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://cnt-a5a0258f-4536-4eeb-a60d-38988b6ce379.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+KITS_PATH = "/api/v1/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,24 @@
+import configuration
+import requests
+import data
+
+def create_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers.copy())
+
+def auth_token_return():
+    response = create_new_user(data.user_body.copy())
+    token = response.json()['authToken']
+    return token
+
+def post_new_client_kit(kit_body):
+    token = auth_token_return()
+    Authorization = {
+        "Content-Type": "application/json",
+        "Authorization": f'Bearer {token}'
+    }
+    return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
+                         headers = Authorization, json=kit_body)
+
+

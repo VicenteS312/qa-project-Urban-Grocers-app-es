ee07d8e0c65e420d8914f200c0ee4cb0bcbd55f9
Vicente Salinas <Vicente.Salinas312@gmail.com>
2024-08-08 19:20:13 -0500

    Change of URL

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-31bca08f-8dd1-46de-b470-d79a7a5f5c3b.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-e34f091b-aaa0-4dc5-b14e-550cae9a06a2.containerhub.tripleten-services.com"
 DOC_PATH = "/docs/"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits/"

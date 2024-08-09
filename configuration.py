85bdd59541a139a6b2c41e90cd21f12a63eceaba
Vicente Salinas <Vicente.Salinas312@gmail.com>
2024-08-08 19:07:32 -0500

    Proyecto 7 primera entrega

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-a5a0258f-4536-4eeb-a60d-38988b6ce379.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-31bca08f-8dd1-46de-b470-d79a7a5f5c3b.containerhub.tripleten-services.com"
 DOC_PATH = "/docs/"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits/"

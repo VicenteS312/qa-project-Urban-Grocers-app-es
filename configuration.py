d30303d2f587e9e8d3750b1c3804c6c53b1f37d7
Vicente Salinas <Vicente.Salinas312@gmail.com>
2024-08-09 20:09:33 -0500

    Proyecto 7 segunda version de entrega

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-b073b006-80d5-4161-b274-9141f5268800.containerhub.tripleten-services.com"
+URL_SERVICE = ("https://cnt-ee19d069-0113-4f41-969d-f1d8210f1ff3.containerhub.tripleten-services.com")
 DOC_PATH = "/docs/"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits/"
